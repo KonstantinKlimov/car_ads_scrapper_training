56daa7b9ecf3206ffb0ef433dd953ccb64dea1a5
timoti1 <timoti@tut.by>
2023-07-08 21:52:25 +0300

    added mssql version of the av by scrapper (finder + scrapper itself)

---
+++ cards_finder_av_by_mssql.py
@@ -0,0 +1,138 @@
+import pymssql
+
+from bs4 import BeautifulSoup
+import requests
+import json
+import time
+
+
+start_time = time.time()
+
+headers = requests.utils.default_headers()
+headers.update({
+    'Accept-Encoding': 'gzip, deflate, sdch',
+    'Accept-Language': 'en-US,en;q=0.8',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'TGTG/22.2.1 Dalvik/2.1.0 (Linux; U; Android 9; SM-G955F Build/PPR1.180610.011)',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive'
+})
+
+DEFAULT_HEADER = headers
+SOURCE_ID = "https://cars.av.by"
+PROCESS_DESC = "cards_finder_av_by.py"
+
+
+def get_card_url_list(url, site_url=SOURCE_ID, headers=DEFAULT_HEADER):
+    url_list = []
+
+    page = requests.get(url, headers=headers)
+    if page.status_code == 200:
+        soup = BeautifulSoup(page.text, "html.parser")
+
+        listing_items = soup.find_all("div", class_="listing-item")
+        try:
+            for item in listing_items:
+                item_href = item.find("a", class_="listing-item__link")["href"]
+                url_list.append(site_url + item_href)
+        except:
+            pass
+
+    return url_list
+
+
+def init_db_connection(con, sql_script_path):
+    result_code = 0
+
+    if sql_script_path is not None:
+        cur = con.cursor()
+        with open(sql_script_path) as init_db_file:
+            for sql_stmt in init_db_file.read().split(";"):
+                try:
+                    cur.execute(sql_stmt)
+                except:
+                    result_code = -1
+
+    return result_code
+
+
+def main():
+    with open("config.json") as config_file:
+        configs = json.load(config_file)
+
+    con = pymssql.connect(**configs["mssql_audit_db"], autocommit=True)
+
+    init_db_connection(con, configs.get("mssql_finder_init_db_script"))
+
+    with con:
+        cur = con.cursor()
+
+        cur.execute(
+            f"""
+                insert into process_log(process_desc, [user], host, connection_id)         
+                select N'{PROCESS_DESC}', 
+                       SYSTEM_USER, 
+                       HOST_NAME(),
+                       @@spid;
+            """
+        )
+        cur.execute("select scope_identity() as process_log_id;")
+        process_log_id = cur.fetchone()[0]
+
+        num = 0
+        curr_year = int(time.strftime("%Y", time.gmtime()))
+
+        for year in range(curr_year, 1900, -1):
+            for price_usd in range(0, 500001, 10000):
+                for page_num in range(1, 501):
+                    num += 1
+
+                    group_url = f"{SOURCE_ID}/filter?year[min]={year}&year[max]={year}&price_usd[min]={price_usd}&price_usd[max]={price_usd + 9999}&page={page_num}"
+
+                    print(f"\ntime: {time.strftime('%X', time.gmtime(time.time() - start_time))}, num: {num}, url: {group_url}")
+
+                    card_url_list = get_card_url_list(group_url)
+                    if card_url_list == []:
+                        print(f"time: {time.strftime('%X', time.gmtime(time.time() - start_time))}, no cards found")
+                        break
+
+                    cur.execute(f"insert into ad_groups(group_url, process_log_id) values('{group_url}', {process_log_id});")
+                    cur.execute("select scope_identity() as ad_group_id;")
+                    ad_group_id = cur.fetchone()[0]
+
+                    for card_url in card_url_list:
+                        cur.execute(
+                            f"""
+                                with cte_new_card
+                                as
+                                ( 
+                                    select N'{card_url[len(SOURCE_ID):]}' as card_url
+                                )
+                                insert into ads(source_id, card_url, ad_group_id, insert_process_log_id)
+                                select N'{SOURCE_ID}' as source_id, 
+                                       card_url, 
+                                       {ad_group_id} as ad_group_id, 
+                                       {process_log_id} as insert_process_log_id
+                                from cte_new_card
+                                where card_url not in (select card_url from ads where source_id=N'{SOURCE_ID}');
+                            """
+                        )
+
+                    if len(card_url_list) < 20:
+                        break
+
+
+        print(f"\nend time (GMT): {time.strftime('%X', time.gmtime())}")
+
+        cur.execute(
+            f"""
+                update process_log 
+                    set end_date = getdate() 
+                where process_log_id = {process_log_id};
+            """
+        )
+
+
+if __name__ == "__main__":
+    main()
---
+++ cards_scrapper_av_by_mssql.py
@@ -0,0 +1,368 @@
+import pymssql
+
+from bs4 import BeautifulSoup
+import requests
+import time
+import json
+
+start_time = time.time()
+
+headers = requests.utils.default_headers()
+# headers.update({
+#     "Accept-Encoding": "gzip, deflate, sdch",
+#     "Accept-Language": "en-US,en;q=0.8",
+#     "Upgrade-Insecure-Requests": "1",
+#     "User-Agent": "TGTG/22.2.1 Dalvik/2.1.0 (Linux; U; Android 9; SM-G955F Build/PPR1.180610.011)",
+#     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+#     "Cache-Control": "max-age=0",
+#     "Connection": "keep-alive"
+# })
+
+DEFAULT_HEADER = headers
+SOURCE_ID = "https://cars.av.by"
+PROCESS_DESC = "cards_scrapper_av_by.py"
+MIN_RESCRAP_TIME = 24
+
+
+def get_info_from_next_data(text):
+    next_data = json.loads(text)
+
+    advert = next_data["props"]["initialState"]["advert"]["advert"]
+
+    advert_id = advert["id"]
+    publicUrl = advert["publicUrl"]
+
+    # photos =  {}
+    # photos["id"] = advert_id
+    # photos["publicUrl"] = publicUrl
+    # photos["photos"] = advert["photos"].copy()
+
+    # properties = {}
+    # properties["id"] = advert_id
+    # properties["publicUrl"] = publicUrl
+    # properties["properties"] = advert["properties"].copy()
+
+    similarAdverts = [{"id": ad["id"], "publicUrl": ad["publicUrl"]} for ad in next_data["props"]["initialState"]["advert"]["similarAdverts"]]
+
+    return advert, similarAdverts
+
+
+def get_parsed_card(url, debug=0, headers=DEFAULT_HEADER):
+    card_dict = {}
+
+    page = requests.get(url, headers=headers)
+    # if debug:
+    #     print(page.status_code,"\n")
+
+    if page.status_code == 200:
+        page_text = page.text.replace("<!-- -->", "")
+        soup = BeautifulSoup(page_text, "html.parser")
+
+        card = soup.find("div", class_="card")
+        # print(card,"\n")
+
+        card_gallery = card.find("div", class_="gallery__stage-shaft")
+        card_dict["gallery"] = []
+        # if debug:
+        #     print("Галлерея")
+        try:
+            for div_img in card_gallery.find_all("div", class_="gallery__frame"):
+                img = div_img.find("img")
+                # if debug:
+                #     print(img["data-srcset"])
+                card_dict["gallery"].append(img["data-srcset"].split()[0])
+        except:
+            pass
+
+        try:
+            card_title = card.find(class_="card__title")
+            card_dict["title"] = card_title.text
+        except:
+            card_dict["title"] = ""
+
+        # if debug:
+        #     print(f"card_title: {card_title.text}")
+
+        card_price_primary = card.find(class_="card__price-primary")
+        # if debug:
+        #     print(f"card_price_primary: {card_price_primary.text}")
+        try:
+            card_dict["price_primary"] = card_price_primary.text
+        except:
+            card_dict["price_primary"] = ""
+
+        card_price_secondary = card.find(class_="card__price-secondary")
+        # if debug:
+        #     print(f"card__price-secondary: {card_price_secondary.text}")
+        try:
+            card_dict["price_secondary"] = card_price_secondary.text
+        except:
+            card_dict["price_secondary"] = ""
+
+        card_comment = card.find("div", class_="card__comment-text")
+        # if debug:
+        #     print(f"card_comment: {card_comment.text}")
+        try:
+            card_dict["comment"] = card_comment.get_text(separator="|", strip=True).replace("\n", "|")
+        except:
+            card_dict["comment"] = ""
+
+        card_location = card.find("div", class_="card__location")
+        try:
+            card_dict["location"] = card_location.text
+        except:
+            card_dict["location"] = ""
+
+        labels = []
+        card_labels = card.find("div", class_="card__labels")
+        try:
+            # check why does exception happen
+            for div in card_labels.find_all("div"):
+                if div.has_attr('class') and len(div['class']) > 1 and div['class'][1] in ["badge--top", "badge--parts", "badge--wreck", "badge--vin", "badge--new"]:
+                    if  div['class'][1] == "badge--top":
+                        labels += ["Top"]
+                    else:
+                        labels += [div.text]
+        except:
+            pass
+        card_dict["labels"] = "|".join(labels)
+
+        try:
+            card_params = card.find("div", class_="card__params")
+            card_dict["description"] = card_params.text
+        except:
+            card_dict["description"] = ""
+
+        try:
+            card_description = card.find("div", class_="card__description")
+            card_dict["description"] += (" | " if card_dict["description"] != "" else "") + card_description.text
+        except:
+            pass
+
+        # if debug:
+        #     print(f"card_description: {card_params.text}\n{card_description.text}")
+
+        try:
+            card_exchange = card.find(class_="card__exchange-title")
+            card_dict["exchange"] = card_exchange.text
+        except:
+            card_dict["exchange"] = ""
+
+        # if debug:
+        #     print(f"card_exchange: {card_exchange.text}")
+
+
+        card_options = card.find(class_="card__options-wrap")
+        card_dict["options"] = []
+        # print(card_options)
+
+        try:
+            for section in card_options.find_all("div", class_="card__options-section"):
+                section_dict = {}
+
+                category = section.find(class_="card__options-category")
+                # if debug:
+                #     print(f"category: {category.text}")
+                section_dict["category"] = category.text
+
+                section_dict["items"] = []
+                for option in section.find_all(class_="card__options-item"):
+                    # if debug:
+                    #     print(f"   - {option.text}")
+                    section_dict["items"].append(option.text)
+
+                card_dict["options"].append(section_dict)
+        except:
+            pass
+
+        card_dict["json"] = {}
+        try:
+            next_data = soup.find("script", id="__NEXT_DATA__")
+            advert, similarAdverts = get_info_from_next_data(next_data.text)
+
+            card_dict["json"]["advert"] = advert
+            # card_dict["json"]["photos"] = photos
+            # card_dict["json"]["properties"] = properties
+            card_dict["json"]["similarAdverts"] = similarAdverts
+        except:
+            pass
+        #
+        # if debug:
+        #     print("\n",str(card_dict).replace("\\xa0", " ").replace("\\u2009", " "))
+
+        card_dict["scrap_date"] = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+
+    return card_dict
+
+
+def init_db_connection(con, sql_script_path):
+    result_code = 0
+
+    if sql_script_path is not None:
+        cur = con.cursor()
+        with open(sql_script_path) as init_db_file:
+            for sql_stmt in init_db_file.read().split(";"):
+                try:
+                    cur.execute(sql_stmt)
+                except:
+                    result_code = -1
+
+    return result_code
+
+
+def main():
+    with open("config.json") as config_file:
+        configs = json.load(config_file)
+
+    con = pymssql.connect(**configs["mssql_audit_db"], autocommit=True)
+
+    init_db_connection(con, configs.get("mssql_scrapper_init_db_script"))
+
+    with con:
+        cur = con.cursor()
+
+        cur.execute(
+            f"""
+                insert into process_log(process_desc, [user], host, connection_id)         
+                select N'{PROCESS_DESC}', 
+                       SYSTEM_USER, 
+                       HOST_NAME(),
+                       @@spid;
+            """
+        )
+
+        cur.execute("select scope_identity() as process_log_id;")
+        process_log_id = cur.fetchone()[0]
+
+        num = 0
+        while True:
+            # get new portion of not yet scrapped urls having the same ad_group_id
+            cur.execute(
+                f"""
+                    with cte_cars_com_ad_group_ids
+                    as
+                    (
+                        select distinct ad_group_id
+                        from ads 
+                        where (
+                                ad_status = 0 or 
+                                (ad_status = 2 and datediff(hour, change_status_date, getdate()) > {MIN_RESCRAP_TIME})
+                              ) and source_id = N'{SOURCE_ID}'
+                    )
+                    select floor(rand() * (select max(ad_group_id) from cte_cars_com_ad_group_ids)) as random_ad_group_id;
+                """
+            )
+
+            random_ad_group_id = cur.fetchone()[0]
+            if random_ad_group_id is None:
+                # check if there is what to do
+                cur.execute(
+                    f"""
+                        select *
+                        from ads 
+                        where (
+                                ad_status = 0 or 
+                                (ad_status = 2 and datediff(hour, change_status_date, getdate()) > {MIN_RESCRAP_TIME})
+                              ) and source_id = N'{SOURCE_ID}';
+                    """
+                )
+                if cur.rowcount > 0:
+                    continue
+                else:
+                    break
+
+            cur.execute(
+                f"""
+                        with cte_random_group
+                        as
+                        (
+                            select top 1 
+                                   ad_group_id as ad_group_id
+                            from ads
+                            where (
+                                    ad_status = 0 or 
+                                    (ad_status = 2 and datediff(hour, change_status_date, getdate()) > {MIN_RESCRAP_TIME})
+                                  ) and 
+                                  source_id = N'{SOURCE_ID}' and
+                                  ad_group_id >= {random_ad_group_id}
+                        )
+                        select a.ads_id, concat(a.source_id, a.card_url) as url, g.group_url 
+                        from ads a
+                        join ad_groups g on a.ad_group_id = g.ad_group_id
+                        join cte_random_group rg on g.ad_group_id = rg.ad_group_id    
+                        where a.ad_status = 0 or 
+                              (ad_status = 2 and datediff(hour, change_status_date, getdate()) > {MIN_RESCRAP_TIME});                    
+                    """
+            )
+            if cur.rowcount == 0:
+                break
+
+            records_fetched = cur.fetchall()
+
+            for ads_id, url, group_url in records_fetched:
+                num += 1
+
+                url_parts = url.split("?")
+
+                parsed_card = {}
+                ad_status = None
+                try:
+                    if len(url_parts) == 1:
+                        parsed_card = get_parsed_card(url)
+                except:
+                    # error when parsing the card (url)
+                    ad_status = -1
+
+                card = '{}'
+                if parsed_card != {}:
+                    # successfully parsed the card (url)
+                    ad_status = 2
+
+                    card = f"{parsed_card}" \
+                        .replace("\\xa0", " ") \
+                        .replace("\\u2009", " ") \
+                        .replace("\\u2013", "-") \
+                        .replace("\\u2026", "") \
+                        .replace("'", '"')
+
+                try:
+                    print(f"{time.strftime('%X', time.gmtime(time.time() - start_time))}, {ad_status}, num: {num}, ads_id: {ads_id}, year: {parsed_card['description'][:4]}, card size: {len(card)}, {url}")
+
+                    sql_string = f"""
+                            update ads
+                               set ad_status = {ad_status},
+                                   change_status_date = getdate(),
+                                   change_status_process_log_id = {process_log_id},
+                                   card = N'{card}'
+                            where ads_id = {ads_id};
+                        """
+                    cur.execute(sql_string)
+                except:
+                    if card == '{}':
+                        ad_status = 1
+                    else:
+                        ad_status = -1
+
+                    print(f"{time.strftime('%X', time.gmtime(time.time() - start_time))}, {ad_status}, num: {num}, ads_id: {ads_id}, year: -, card size: {len(card)}, {url}")
+
+                    sql_string = f"""
+                            update ads
+                               set ad_status = {ad_status},
+                                   change_status_date = getdate(),
+                                   change_status_process_log_id = {process_log_id}                                   
+                            where ads_id = {ads_id};
+                        """
+                    cur.execute(sql_string)
+
+
+        cur.execute(
+            f"""
+                update process_log 
+                    set end_date = getdate() 
+                where process_log_id = {process_log_id};
+            """
+        )
+
+
+if __name__ == "__main__":
+    main()
